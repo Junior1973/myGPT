23f481dc7050e17368d7799de247f576ce395a56
Junior1973 <pereshfrilans@mail.ru>
2023-07-13 11:11:36 +0800

    fail

--- function.php
+++ function.php
@@ -10,10 +10,10 @@ add_action( 'wp_enqueue_scripts', function () {
 	// wp_enqueue_style( 'animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' );
 	// wp_enqueue_style( 'style', get_template_directory_uri(). '/assets/css/style.css' );
 
-	// wp_deregister_script( 'jquery' );
-	// wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
 
-	// wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'jquery' );
 	// wp_enqueue_script( 'slick.min', get_template_directory_uri() . '/assets/js/slick.min.js', array('jquery'), 'null', true );
 	// wp_enqueue_script( 'wow.min', get_template_directory_uri() . '/assets/js/wow.min.js', array('jquery'), 'null', true );
 	// wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 'null', true );
